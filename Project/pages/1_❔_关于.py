76cfd455bf79146fb24272ab433cffaa5f232521
huanxingke <1519657825@qq.com>
2023-03-04 13:00:51 +0800

    2023/03/04

--- Project/pages/1_❔_关于.py
+++ Project/pages/1_❔_关于.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 import requests
 
-from utils.config import menu_items, work_path
+from utils.config import menu_items
 from utils.initUserConfig import initUserConfig
 
 # ---------- Start:每页基础配置 ---------- #
@@ -17,7 +17,7 @@ if init_result:
 
     # 加载 README.md
     with st.spinner("正在加载本站信息..."):
-        readme_md = os.path.join(work_path, "README.md")
+        readme_md = os.path.join(st.session_state.work_path, "README.md")
         with open(readme_md, "r", encoding="utf-8") as fp:
             st.markdown(fp.read())
             st.image("https://www.scut.edu.cn/_upload/article/images/93/f1/da8bef494e929b2303b75fcae24a/5239d956-fc69-4d30-bb16-80f5fada82ea.jpg")
