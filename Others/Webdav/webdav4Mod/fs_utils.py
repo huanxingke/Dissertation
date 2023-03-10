76225743b8ae86ec43f439863fe85427c73f1d7c
欢行客 <1519657825@qq.com>
2023-03-10 23:55:28 +0800

    Delete Others directory

--- Others/Webdav/webdav4Mod/fs_utils.py
+++
@@ -1,5 +0,0 @@
-"""Common file utilities."""
-
-from httpx._utils import (  # noqa: F401, pylint: disable=unused-import
-    peek_filelike_length,
-)
--- Others/Webdav/webdav4Mod/types.py
+++
@@ -1,9 +0,0 @@
-"""Types."""
-
-# pylint: disable=unused-import
-from httpx import Response as HTTPResponse  # noqa: F401, pragma: no cover
-from httpx._types import (  # noqa: F401, pragma: no cover
-    AuthTypes,
-    HeaderTypes,
-    URLTypes,
-)
